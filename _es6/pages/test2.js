a87fad38183b2d07924bd159389b00861c81f734
zhouxianglin <zhouxianglin@192.168.0.145>
2017-04-26 10:12:00 +0800

    mod

--- _es6/pages/test2.js
+++
@@ -1,9 +0,0 @@
-// import Test from "../module/test/testModule.js";
-
-// require("../../../_sass/pages/style.scss");
-
-// var test = new Test();
-// test.b = 1;
-// console.log(test.b);
-
-console.log(param);
